78cb913bbbdc585b11238967aebec8330664ba7b
Dan Govan <danielgovan@gmail.com>
2020-12-28 12:26:23 +0000

    Firebase startpoint

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>LightByDan</h1>
+      <h2>A different light</h2>
+      <p>Welcome</p>
+    </div>
+  );
+};
+
+export default Title;
