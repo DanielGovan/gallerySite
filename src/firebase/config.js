2dcd186759d8d41ea8c5943c2c346fd7f0a1e4cc
Dan Govan <danielgovan@gmail.com>
2020-12-29 13:19:51 +0000

    env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/storage";
 import "firebase/firestore";
+import "firebase/auth";
 
 // <!-- The core Firebase JS SDK is always required and must be listed first -->
 // <script src="https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js"></script>
@@ -12,21 +13,23 @@ import "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-  apiKey: "AIzaSyCJOyzfE46rG_SUOlr-k62BSP4JUdq_4Tw",
-  authDomain: "lightbydan-1569325510991.firebaseapp.com",
-  projectId: "lightbydan-1569325510991",
-  storageBucket: "lightbydan-1569325510991.appspot.com",
-  messagingSenderId: "891128779497",
-  appId: "1:891128779497:web:89af3a87ce7ab899e55678",
-  measurementId: "G-V5ZFGBLTW5",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 // firebase.analytics();
 
+const projectAuth = firebase.auth();
 const projectStorage = firebase.storage();
 const projectFirestore = firebase.firestore();
 
 const timestamp = firebase.firestore.FieldValue.serverTimestamp;
 
-export { projectStorage, projectFirestore, timestamp };
+export { projectStorage, projectFirestore, projectAuth, timestamp };
